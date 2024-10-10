3ee6172d2f0ef3e51085811419a00fda6aa1a5e2
Prasannaa E <53602368+6382737054@users.noreply.github.com>
2024-10-10 15:08:47 +0530

    Initial commit

--- src/Pages/GetInvolved.jsx
+++ src/Pages/GetInvolved.jsx
@@ -27,7 +27,7 @@ const GetInvolved = () => {
           title: "Global Brotherhood",
           description: "Scouting is a worldwide movement that brings together  people from diverse backgrounds, fostering understanding and peace across cultures.",
           details: [
-            "54 million Scouts across 170 countries",
+            "54 million Scouts across 175 countries",
             "Annual events like World Scout  and JOTA-JOTI",
             "International exchange programs and partnerships"
           ]
